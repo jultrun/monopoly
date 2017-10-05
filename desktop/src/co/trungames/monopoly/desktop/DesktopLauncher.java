aac574f76130e02e8f8f2e4264e458050ee24475
jultrun <jultrun@github.com>
2017-10-05 14:01:19 -0500

    rutas relativas fin

--- desktop/src/co/trungames/monopoly/desktop/DesktopLauncher.java
+++ desktop/src/co/trungames/monopoly/desktop/DesktopLauncher.java
@@ -1,16 +1,12 @@
-package co.trungames.monopoly.game.desktop;
+package co.trungames.monopoly.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-
-import co.trungames.monopoly.game.Monopoly;
-import co.trungames.monopoly.game.Monopoly2;
-
+import co.trungames.monopoly.Monopoly2;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		new LwjglApplication(new Monopoly2(), config);
-		new Monopoly().main(arg);;
 	}
 }
