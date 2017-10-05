b49497181bfca247ddd4fb38143cd159e0b91a4a
jultrun <jultrun@github.com>
2017-10-05 01:04:32 -0500

    gradlew

--- desktop/src/co/trungames/monopoly/game/desktop/DesktopLauncher.java
+++ desktop/src/co/trungames/monopoly/game/desktop/DesktopLauncher.java
@@ -2,10 +2,13 @@ package co.trungames.monopoly.game.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+
+import co.trungames.monopoly.game.Monopoly;
 import co.trungames.monopoly.game.Monopoly2;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new Monopoly2(), config);
+		//new LwjglApplication(new Monopoly2(), config);
+		new Monopoly().main(arg);;
 	}
 }
